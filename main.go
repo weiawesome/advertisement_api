d7d7f13bd1c97ed238bb05b63f52d854173ba0a5
tcweeei <open891013@gmail.com>
2024-02-29 15:42:33 +0800

    Explain code.

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+/*
+The main function to start the application.
+At first, it initializes all setting include logger, maps, singleflight, database.
+Then it starts the service by the framework, gin-gonic/gin.
+*/
 package main
 
 import (
@@ -7,24 +12,34 @@ import (
 )
 
 func main() {
+	// initialize the logger.
 	utils.InitLogger()
+
+	// initialize the maps include gender-map, country-map and platform-map.
 	utils.InitMaps()
+
+	// initialize the singleflight instance
 	utils.InitSingleFLight()
 
+	// initialize the sql-db connection. return and log error when connect fail.
 	if err := utils.InitDB(); err != nil {
-		//utils.LogFatal(utils.LogData{Event: "Failed to connect to the database", User: "system", Details: err.Error()})
+		utils.LogFatal(err.Error())
 		return
 	}
+	// close sql-db connection when application turn off. log error when close fail.
 	defer func() {
 		err := utils.CloseDB()
 		if err != nil {
 			utils.LogFatal(err.Error())
 		}
 	}()
+
+	// initialize the redis connection. return and log error when connect fail.
 	if err := utils.InitRedis(); err != nil {
-		//utils.LogFatal(utils.LogData{Event: "Failed to connect to the redis", User: "system", Details: err.Error()})
+		utils.LogFatal(err.Error())
 		return
 	}
+	// close redis connection when application turn off. log error when close fail.
 	defer func() {
 		err := utils.CloseRedis()
 		if err != nil {
@@ -32,11 +47,15 @@ func main() {
 		}
 	}()
 
+	// set mode to the release mode.
 	gin.SetMode(gin.ReleaseMode)
-	r := routes.InitRoutes()
-	err := r.Run()
 
+	// initialize all the routes in the server.
+	r := routes.InitRoutes()
+	// start the service. log error when start fail.
+	err := r.Run()
 	if err != nil {
+		utils.LogFatal(err.Error())
 		return
 	}
 }
