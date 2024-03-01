3672d7a253de9629332fc5a675d0f923f90b2e63
tcweeei <open891013@gmail.com>
2024-03-01 12:51:20 +0800

    Edit with error case too much country.

--- api/middleware/url_parameter/advertisement/country.go
+++ api/middleware/url_parameter/advertisement/country.go
@@ -8,15 +8,32 @@ import (
 )
 
 func validateCountry(country string) bool {
-	return country == utils.GetDefaultCountry() || utils.GetCountriesMap()[country]
+	return utils.GetCountriesMap()[country]
 }
 
 func MiddlewareCountry() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		country := c.DefaultQuery("country", utils.GetDefaultCountry())
+		countries, exists := c.Request.URL.Query()["country"]
 
-		if status := validateCountry(country); status == false {
-			e := failure.ClientError{Reason: "country's parameter validate error"}
+		var country string
+
+		if !exists {
+			country = utils.GetDefaultCountry()
+		} else if len(countries) == 0 {
+			e := failure.ClientError{Reason: "country's parameter error, value empty error"}
+			c.JSON(http.StatusBadRequest, e)
+			c.Abort()
+			return
+		} else if len(countries) == 1 {
+			country = countries[0]
+			if status := validateCountry(country); status == false {
+				e := failure.ClientError{Reason: "country's parameter validate error"}
+				c.JSON(http.StatusBadRequest, e)
+				c.Abort()
+				return
+			}
+		} else {
+			e := failure.ClientError{Reason: "country's parameter validate error, too much country parameters"}
 			c.JSON(http.StatusBadRequest, e)
 			c.Abort()
 			return
