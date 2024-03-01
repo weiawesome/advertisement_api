dc3cdf9daa7e9e5c87ddccd9e50c7d642030d49d
tcweeei <open891013@gmail.com>
2024-03-01 21:38:31 +0800

    Delete not use function.

--- utils/utils.go
+++ utils/utils.go
@@ -1,3 +1,7 @@
+/*
+Some tools including getting today, getting location dsn setting.
+*/
+
 package utils
 
 import (
@@ -5,19 +9,27 @@ import (
 	"time"
 )
 
+// GetToday is a function to get today's date
 func GetToday() time.Time {
+	// get the location setting from the environment
 	location, err := time.LoadLocation(EnvLocation())
+
+	// if failed to load location time, it will use local time to return
 	if err != nil {
 		now := time.Now()
 		today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, location)
 		return today
 	}
+
+	// return the time with location setting
 	now := time.Now().In(location)
 	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, location)
 	return today
 }
 
+// GetSqlDsnLocation is to edit SQL DSN setting
 func GetSqlDsnLocation() string {
+	// get the location and replace word "/" into "%2F"
 	location := EnvLocation()
 	return strings.Replace(location, "/", "%2F", 1)
 }
