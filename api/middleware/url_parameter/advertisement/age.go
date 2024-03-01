b557ed9714fa8126d6bc9a2663604d069d6821b3
tcweeei <open891013@gmail.com>
2024-03-01 12:50:24 +0800

    Edit with error case too much ages.

--- api/middleware/url_parameter/advertisement/age.go
+++ api/middleware/url_parameter/advertisement/age.go
@@ -1,3 +1,7 @@
+/*
+The middleware of gin to validate and parse parameter in url about age
+*/
+
 package advertisement
 
 import (
@@ -9,37 +13,50 @@ import (
 	"strconv"
 )
 
+// parse and validate the age parameter
 func parseValidateAge(age string) (int, error) {
-	if age == "" {
-		return utils.GetDefaultAge(), nil
-	} else {
-
-		age, err := strconv.Atoi(age)
-		if err != nil {
-			return age, err
-		}
-
-		if age < utils.GetMinAge() || age > utils.GetMaxAge() {
-			return age, errors.New("invalidate age")
-		} else {
-			return age, err
-		}
+	ageInt, err := strconv.Atoi(age)
+	if err != nil {
+		return ageInt, err
 	}
+
+	if ageInt < utils.GetMinAge() || ageInt > utils.GetMaxAge() {
+		return ageInt, errors.New("invalidate age")
+	}
+
+	return ageInt, nil
 }
 
 func MiddlewareAge() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		age := c.DefaultQuery("age", "")
+		ages, exists := c.Request.URL.Query()["age"]
 
-		ageInt, err := parseValidateAge(age)
-		if err != nil {
-			e := failure.ClientError{Reason: "age's parameter parse error, " + err.Error()}
+		var age int
+
+		if !exists {
+			age = utils.GetDefaultAge()
+		} else if len(ages) == 0 {
+			e := failure.ClientError{Reason: "age's parameter error, value empty error"}
+			c.JSON(http.StatusBadRequest, e)
+			c.Abort()
+			return
+		} else if len(ages) == 1 {
+			ageInt, err := parseValidateAge(ages[0])
+			age = ageInt
+			if err != nil {
+				e := failure.ClientError{Reason: "age's parameter parse error, " + err.Error()}
+				c.JSON(http.StatusBadRequest, e)
+				c.Abort()
+				return
+			}
+		} else {
+			e := failure.ClientError{Reason: "age's parameter parse error, too much age parameters"}
 			c.JSON(http.StatusBadRequest, e)
 			c.Abort()
 			return
 		}
 
-		c.Set("age", ageInt)
+		c.Set("age", age)
 		c.Next()
 	}
 }
