e970b5f9aaeb3a806df8f3c1731cf4d3b5f625b2
tcweeei <open891013@gmail.com>
2024-03-01 15:45:54 +0800

    Explain code.

--- api/handler/advertisement/add_advertisement.go
+++ api/handler/advertisement/add_advertisement.go
@@ -1,3 +1,9 @@
+/*
+The handler with service to handle about add advertisement.
+At first, it will get value form context then throw the value to the service.
+Finally, according to the value rerun from service then return different response to the client.
+*/
+
 package advertisement
 
 import (
@@ -11,11 +17,14 @@ import (
 	"net/http"
 )
 
+// HandlerAddAdvertisement is the handler for adding advertisement.
 type HandlerAddAdvertisement struct {
-	Service advertisementService.ServiceAddAdvertisement
+	Service advertisementService.ServiceAddAdvertisement // service for adding advertisement
 }
 
+// Handle to handle the request about adding advertisement
 func (h *HandlerAddAdvertisement) Handle(c *gin.Context) {
+	// get the request body from context. if failed, it will return error
 	advertisementData, ok := utilsHandler.GetFromContext[advertisement.AddAdvertisementRequest](c, "data")
 	if !ok {
 		e := failure.ServerError{Reason: "Data not found in context"}
@@ -23,16 +32,23 @@ func (h *HandlerAddAdvertisement) Handle(c *gin.Context) {
 		return
 	}
 
+	// use the data to service and get the return status
 	result, err := h.Service.Add(advertisementData)
+
 	if err == nil {
+		// success to add the advertisement
 		c.JSON(http.StatusOK, result)
 	} else if errors.As(err, &failure.DayLimitError{}) {
+		// fail to add advertisement due to reaching daily limit
 		e := failure.ServerError{Reason: "Day limit error, " + err.Error()}
 		c.JSON(http.StatusTooManyRequests, e)
 	} else if errors.As(err, &failure.DurationLimitError{}) {
+		// fail to add advertisement due to reaching duration limit
 		e := failure.ServerError{Reason: "Duration limit error, " + err.Error()}
 		c.JSON(http.StatusConflict, e)
 	} else {
+		// fail to add advertisement due to unknown reason
+		// log its error reason and return server error
 		go utils.LogError(err.Error())
 		e := failure.ServerError{Reason: "Server error, " + err.Error()}
 		c.JSON(http.StatusInternalServerError, e)
--- api/handler/advertisement/get_advertisement.go
+++ api/handler/advertisement/get_advertisement.go
@@ -1,3 +1,9 @@
+/*
+The handler with service to handle about get advertisements.
+At first, it will get value form context then throw the value to the service.
+Finally, according to the value rerun from service then return different response to the client.
+*/
+
 package advertisement
 
 import (
@@ -9,29 +15,35 @@ import (
 	"net/http"
 )
 
+// HandlerGetAdvertisement is the handler for getting advertisement.
 type HandlerGetAdvertisement struct {
-	Service advertisement.ServiceGetAdvertisement
+	Service advertisement.ServiceGetAdvertisement // service for getting advertisement
 }
 
+// Handle to handle the request about getting advertisement
 func (h *HandlerGetAdvertisement) Handle(c *gin.Context) {
+	// get the age from context. if failed, it will return error
 	age, ageOk := utilsHandler.GetFromContext[int](c, "age")
 	if !ageOk {
 		e := failure.ServerError{Reason: "Age not found in context"}
 		c.JSON(http.StatusInternalServerError, e)
 		return
 	}
+	// get the country from context. if failed, it will return error
 	country, countryOk := utilsHandler.GetFromContext[string](c, "country")
 	if !countryOk {
 		e := failure.ServerError{Reason: "Country not found in context"}
 		c.JSON(http.StatusInternalServerError, e)
 		return
 	}
+	// get the gender from context. if failed, it will return error
 	gender, genderOk := utilsHandler.GetFromContext[string](c, "gender")
 	if !genderOk {
 		e := failure.ServerError{Reason: "Gender not found in context"}
 		c.JSON(http.StatusInternalServerError, e)
 		return
 	}
+	// get the platform from context. if failed, it will return error
 	platform, platformOk := utilsHandler.GetFromContext[string](c, "platform")
 	if !platformOk {
 		e := failure.ServerError{Reason: "Platform not found in context"}
@@ -39,12 +51,14 @@ func (h *HandlerGetAdvertisement) Handle(c *gin.Context) {
 		return
 	}
 
+	// get the offset from context. if failed, it will return error
 	offset, offsetOk := utilsHandler.GetFromContext[int](c, "offset")
 	if !offsetOk {
 		e := failure.ServerError{Reason: "Offset not found in context"}
 		c.JSON(http.StatusInternalServerError, e)
 		return
 	}
+	// get the limit from context. if failed, it will return error
 	limit, limitOk := utilsHandler.GetFromContext[int](c, "limit")
 	if !limitOk {
 		e := failure.ServerError{Reason: "Limit not found in context"}
@@ -52,10 +66,15 @@ func (h *HandlerGetAdvertisement) Handle(c *gin.Context) {
 		return
 	}
 
+	// use the data to service and get the return status
 	result, err := h.Service.Get(utilsHandler.GetFullUrl(c), age, country, gender, platform, offset, limit)
+
 	if err == nil {
+		// success to get the advertisements
 		c.JSON(http.StatusOK, result)
 	} else {
+		// fail to add advertisement due to unknown reason
+		// log its error reason and return server error
 		go utils.LogError(err.Error())
 		e := failure.ServerError{Reason: "Server error, " + err.Error()}
 		c.JSON(http.StatusInternalServerError, e)
