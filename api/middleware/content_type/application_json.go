1cee92e1e5d3e341115eaf927c54af1d301cf43d
tcweeei <open891013@gmail.com>
2024-03-01 11:52:41 +0800

    Explain code.

--- api/middleware/content_type/application_json.go
+++ api/middleware/content_type/application_json.go
@@ -1,3 +1,7 @@
+/*
+The middleware of gin to check content-type is application/json or not
+*/
+
 package content_type
 
 import (
@@ -6,11 +10,13 @@ import (
 	"net/http"
 )
 
+// MiddlewareApplicationJson to check the content-type
 func MiddlewareApplicationJson() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		// get the content-type
 		contentType := c.ContentType()
 
+		// when content-type is not application/json then return error
 		if contentType != "application/json" {
 			e := failure.ClientError{Reason: "Content-Type must be application/json not " + contentType}
 			c.JSON(http.StatusUnsupportedMediaType, e)
@@ -18,6 +24,7 @@ func MiddlewareApplicationJson() gin.HandlerFunc {
 			return
 		}
 
+		// go to the next handler
 		c.Next()
 	}
 }
