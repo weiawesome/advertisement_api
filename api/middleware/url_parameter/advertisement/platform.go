c21fe769c1dc9b70f9a6e516ec02d86ee4595692
tcweeei <open891013@gmail.com>
2024-03-01 12:50:55 +0800

    Edit with error case too much platform.

--- api/middleware/url_parameter/advertisement/platform.go
+++ api/middleware/url_parameter/advertisement/platform.go
@@ -3,22 +3,37 @@ package advertisement
 import (
 	"advertisement_api/api/response/failure"
 	"advertisement_api/utils"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func validatePlatform(platform string) bool {
-	fmt.Println(utils.GetPlatformsMap()[platform])
-	return platform == utils.GetDefaultPlatform() || utils.GetPlatformsMap()[platform]
+	return utils.GetPlatformsMap()[platform]
 }
 
 func MiddlewarePlatform() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		platform := c.DefaultQuery("platform", utils.GetDefaultPlatform())
+		platforms, exists := c.Request.URL.Query()["platform"]
 
-		if status := validatePlatform(platform); status == false {
-			e := failure.ClientError{Reason: "platform's parameter validate error"}
+		var platform string
+
+		if !exists {
+			platform = utils.GetDefaultPlatform()
+		} else if len(platforms) == 0 {
+			e := failure.ClientError{Reason: "platform's parameter error, value empty error"}
+			c.JSON(http.StatusBadRequest, e)
+			c.Abort()
+			return
+		} else if len(platforms) == 1 {
+			platform = platforms[0]
+			if status := validatePlatform(platform); status == false {
+				e := failure.ClientError{Reason: "platform's parameter validate error"}
+				c.JSON(http.StatusBadRequest, e)
+				c.Abort()
+				return
+			}
+		} else {
+			e := failure.ClientError{Reason: "platform's parameter validate error, too much platform parameters"}
 			c.JSON(http.StatusBadRequest, e)
 			c.Abort()
 			return
