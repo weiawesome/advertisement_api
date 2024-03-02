61e7cc3c3f5dbe0fcaf9ee5380d7f676d2f689da
tcweeei <open891013@gmail.com>
2024-03-02 21:12:36 +0800

    Replace with interface to realize.

--- internal/repository/sql/repository.go
+++ internal/repository/sql/repository.go
@@ -5,17 +5,25 @@ The repository for the sql.
 package sql
 
 import (
+	"advertisement_api/api/request/advertisement"
+	"advertisement_api/internal/repository/model"
 	"advertisement_api/utils"
 	"gorm.io/gorm"
 )
 
-// Repository is the structure of the sql repository
-type Repository struct {
+// Repository is the interface of sql repository
+type Repository interface {
+	GetAdvertisement(Age int, Country string, Gender string, Platform string, Offset int, Limit int) ([]model.Advertisement, error)
+	AddAdvertisement(data advertisement.AddAdvertisementRequest) error
+}
+
+// repository is the structure of the sql repository
+type repository struct {
 	db *gorm.DB // database connection by gorm
 }
 
 // NewRepository is the constructor for the sql repository
-func NewRepository() *Repository {
+func NewRepository() Repository {
 	// return a new sql repository with the sql database client
-	return &Repository{db: utils.GetDB()}
+	return &repository{db: utils.GetDB()}
 }
