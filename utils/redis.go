a8c5e56200c37281b931302f5cf222c82aee6844
tcweeei <open891013@gmail.com>
2024-03-02 11:50:24 +0800

    Delete not use function.

--- utils/redis.go
+++ utils/redis.go
@@ -1,26 +1,42 @@
+/*
+There is a redis client instance.
+If program start, it will initialize the instance and try to connect redis database.
+Furthermore, get function and close function is to get redis client and close redis connection.
+*/
+
 package utils
 
 import (
 	"github.com/redis/go-redis/v9"
 )
 
+// redis client instance
 var redisClient *redis.Client
 
+// InitRedis is to initialize connection to redis database
 func InitRedis() error {
+	// get the password, address and db from environment
 	Password := EnvRedisPassword()
 	Address := EnvRedisAddress()
 	Db := EnvRedisDb()
+
+	// try to connect with redis database
 	opt, err := redis.ParseURL("redis://:" + Password + "@" + Address + "/" + Db)
 	if err != nil {
 		return err
 	}
+
+	// if success to connect redis database, it will make a new a redis client instance
 	redisClient = redis.NewClient(opt)
 	return nil
 }
 
+// GetRedisClient is to get the redis client
 func GetRedisClient() *redis.Client {
 	return redisClient
 }
+
+// CloseRedis is to close redis database connection
 func CloseRedis() error {
 	if redisClient == nil {
 		return nil
--- utils/singleflight.go
+++ utils/singleflight.go
@@ -1,13 +1,22 @@
+/*
+The singleflight is to handle with cache-invalid.
+There is a singleflight instance. If program start, it will initialize the instance.
+There is a get function making other function to use it.
+*/
+
 package utils
 
 import singleflight "golang.org/x/sync/singleflight"
 
+// the singleflight instance
 var gsf *singleflight.Group
 
+// InitSingleFLight is the function of initialization singleflight
 func InitSingleFLight() {
 	gsf = &singleflight.Group{}
 }
 
+// GetSingleFlight is to get the singleflight instance
 func GetSingleFlight() *singleflight.Group {
 	return gsf
 }
