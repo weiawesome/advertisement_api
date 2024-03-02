254eaacbadcad9920dd1c8d1456de2078a12a7ff
tcweeei <open891013@gmail.com>
2024-03-02 21:12:59 +0800

    Handle the request without parameters.

--- api/handler/utils/full_url.go
+++ api/handler/utils/full_url.go
@@ -4,10 +4,16 @@ This is a tool to get the full url including parameter in query
 
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 // GetFullUrl is to get the full url and all query's parameters
 func GetFullUrl(c *gin.Context) string {
+	// if the query without parameters
+	if c.Request.URL.RawQuery == "" {
+		return c.Request.URL.Path
+	}
 	// return the combination of route and query parameter
-	return c.Request.URL.RawPath + c.Request.URL.RawQuery
+	return c.Request.URL.Path + "?" + c.Request.URL.RawQuery
 }
