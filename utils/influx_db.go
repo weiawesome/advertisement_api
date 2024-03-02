d95213af4c0d674a1ab3e679a73bb4bbb4785d07
tcweeei <open891013@gmail.com>
2024-03-02 12:06:40 +0800

    Delete not use function.

--- utils/influx_db.go
+++ utils/influx_db.go
@@ -1,3 +1,8 @@
+/*
+There an influxdb client instance.
+Furthermore, there is a constructor for the influxdb connection.
+*/
+
 package utils
 
 import (
@@ -7,37 +12,54 @@ import (
 	"time"
 )
 
+// InfluxDBClient have a influxdb client
 type InfluxDBClient struct {
 	Client influxdb2.Client
 }
 
+// influxdb connection instance
 var influxDBClient InfluxDBClient
 
+// NewInfluxDBClient is a constructor for the influxdb client
 func NewInfluxDBClient() InfluxDBClient {
+	// connection has been existed
 	if influxDBClient.Client != nil {
 		return influxDBClient
 	}
+
+	// get the address and token from environment
 	url := EnvInfluxDbAddress()
 	token := EnvInfluxDbToken()
+
+	// try to connect the influxdb
 	influxDBClient.Client = influxdb2.NewClient(url, token)
 	return influxDBClient
 }
 
+/*
+A hook for zerolog to make it available to log information into influxdb.
+*/
+
+// InfluxDBHook is the hook structure for zerolog
 type InfluxDBHook struct {
 	Client InfluxDBClient
 }
 
+// Run is to write the log information
 func (h InfluxDBHook) Run(e *zerolog.Event, level zerolog.Level, message string) {
+	// set the save point
 	point := influxdb2.
 		NewPointWithMeasurement("logs").
 		SetTime(time.Now()).
 		AddTag("level", level.String()).
 		AddField("message", message)
 
+	// get the organization and bucket from environment
 	org := EnvInfluxDbOrg()
 	bucket := EnvInfluxDbBucket()
-	writeAPI := h.Client.Client.WriteAPIBlocking(org, bucket)
 
+	// build the write api and write to the influxdb
+	writeAPI := h.Client.Client.WriteAPIBlocking(org, bucket)
 	err := writeAPI.WritePoint(context.Background(), point)
 	if err != nil {
 		LogError(err.Error())
--- utils/mysql.go
+++ utils/mysql.go
@@ -1,3 +1,9 @@
+/*
+There is a gorm db instance connect with mysql database.
+If program start, it will initialize the instance and try to connect mysql database.
+Furthermore, get function and close function is to get mysql client and close mysql connection.
+*/
+
 package utils
 
 import (
@@ -6,35 +12,47 @@ import (
 	"gorm.io/gorm"
 )
 
+// gorm db instance
 var db *gorm.DB
 
+// this is try to connect mysql database
 func connectDB() (*gorm.DB, error) {
+	// get the user, password, address, db name, location from environment
 	user := EnvMySqlUser()
 	password := EnvMySqlPassword()
 	address := EnvMySqlAddress()
 	dbName := EnvMySqlDb()
 	location := GetSqlDsnLocation()
 
+	// build the dsn and try to connect the database
 	dsn := user + ":" + password + "@tcp(" + address + ")/" + dbName + "?charset=utf8mb4&loc=" + location + "&parseTime=True"
 
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
 
+// InitDB is to initialize the db connection
 func InitDB() error {
 	var err error
+
+	// try to connect sql database
 	if db, err = connectDB(); err != nil {
 		return err
 	}
+
+	// try to migrate the model to the database
+	// models including advertisement, age condition, country condition, platform condition and gender condition
 	if err := db.AutoMigrate(model.Advertisement{}, model.AgeCondition{}, model.CountryCondition{}, model.PlatformCondition{}, model.GenderCondition{}); err != nil {
 		return err
 	}
 	return nil
 }
 
+// GetDB is to get the db instance
 func GetDB() *gorm.DB {
 	return db
 }
 
+// CloseDB is to close the mysql database connection
 func CloseDB() error {
 	if db == nil {
 		return nil
