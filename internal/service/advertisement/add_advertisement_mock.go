27a94978bf9c4bef87e6144995c4deb779733b0b
tcweeei <open891013@gmail.com>
2024-03-03 20:32:34 +0800

    Add service's mock.

---
+++ internal/service/advertisement/add_advertisement_mock.go
@@ -0,0 +1,39 @@
+/*
+The service for adding advertisement.
+It has sql repository to realize interaction with database.
+*/
+
+package advertisement
+
+import (
+	advertisementRequest "advertisement_api/api/request/advertisement"
+	advertisementResponse "advertisement_api/api/response/advertisement"
+	"advertisement_api/api/response/failure"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	NormalTitleMock        = "Advertisement's title"
+	DayLimitTitleMock      = "Exceed day limitation"
+	DurationLimitTitleMock = "Exceed duration limitation"
+	UnknownErrorTitleMock  = "Unknown error"
+)
+
+type AddAdvertisementServiceMock struct {
+	mock.Mock
+}
+
+// Add is to get the content from handler and add advertisement by sql repository
+func (m *AddAdvertisementServiceMock) Add(data advertisementRequest.AddAdvertisementRequest) (advertisementResponse.AddAdvertisementResponse, error) {
+	var result advertisementResponse.AddAdvertisementResponse
+	if *data.Title == NormalTitleMock {
+		return result, nil
+	} else if *data.Title == DayLimitTitleMock {
+		return result, failure.DayLimitError{Reason: DayLimitTitleMock}
+	} else if *data.Title == DurationLimitTitleMock {
+		return result, failure.DurationLimitError{Reason: DurationLimitTitleMock}
+	} else if *data.Title == UnknownErrorTitleMock {
+		return result, failure.ServerError{Reason: UnknownErrorTitleMock}
+	}
+	return result, failure.ServerError{Reason: UnknownErrorTitleMock}
+}
