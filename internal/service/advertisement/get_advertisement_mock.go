49e43a19ac6556b8d12a8675859647b924f6fd84
tcweeei <open891013@gmail.com>
2024-03-03 20:32:23 +0800

    Add service's mock.

---
+++ internal/service/advertisement/get_advertisement_mock.go
@@ -0,0 +1,31 @@
+/*
+The service for adding advertisement.
+It has sql repository to realize interaction with database.
+*/
+
+package advertisement
+
+import (
+	advertisementResponse "advertisement_api/api/response/advertisement"
+	"advertisement_api/api/response/failure"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	NormalCountryMock       = "Normal key"
+	UnknownErrorCountryMock = "Unknown error"
+)
+
+type GetAdvertisementServiceMock struct {
+	mock.Mock
+}
+
+// Add is to get the content from handler and add advertisement by sql repository
+func (m *GetAdvertisementServiceMock) Get(Key string, Age int, Country string, Gender string, Platform string, Offset int, Limit int) (advertisementResponse.GetAdvertisementResponse, error) {
+	var result advertisementResponse.GetAdvertisementResponse
+
+	if Country == NormalCountryMock {
+		return result, nil
+	}
+	return result, failure.ServerError{Reason: UnknownErrorCountryMock}
+}
---
+++ utils/env_test.go
@@ -0,0 +1,46 @@
+/*
+All the environment variable can get by following function
+Furthermore, setting default value for all the variable in start of file
+*/
+
+package utils
+
+import (
+	"testing"
+)
+
+// EnvMySqlAddress is to get the environment variable of MYSQL_HOST and MYSQL_PORT
+func TestEnvMySqlAddress(t *testing.T) {}
+
+// EnvMySqlDb is to get the environment variable of MYSQL_DB
+func TestEnvMySqlDb(t *testing.T) {}
+
+// EnvMySqlUser is to get the environment variable of MYSQL_USER
+func TestEnvMySqlUser(t *testing.T) {}
+
+// EnvMySqlPassword is to get the environment variable of MYSQL_PASSWORD
+func TestEnvMySqlPassword(t *testing.T) {}
+
+// EnvRedisAddress is to get the environment variable of REDIS_HOST and REDIS_PORT
+func TestEnvRedisAddress(t *testing.T) {}
+
+// EnvRedisPassword is to get the environment variable of REDIS_PASSWORD
+func TestEnvRedisPassword(t *testing.T) {}
+
+// EnvRedisDb is to get the environment variable of REDIS_DB
+func TestEnvRedisDb(t *testing.T) {}
+
+// EnvInfluxDbAddress is to get the environment variable of INFLUXDB_HOST and INFLUXDB_PORT
+func TestEnvInfluxDbAddress(t *testing.T) {}
+
+// EnvInfluxDbToken is to get the environment variable of INFLUXDB_TOKEN
+func TestEnvInfluxDbToken(t *testing.T) {}
+
+// EnvInfluxDbOrg is to get the environment variable of INFLUXDB_ORG
+func TestEnvInfluxDbOrg(t *testing.T) {}
+
+// EnvInfluxDbBucket is to get the environment variable of INFLUXDB_BUCKET
+func TestEnvInfluxDbBucket(t *testing.T) {}
+
+// EnvLocation is to get the environment variable of LOCATION
+func TestEnvLocation(t *testing.T) {}
