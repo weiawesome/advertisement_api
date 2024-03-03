e52498339547a35681f0b0471366a5ff729c8de5
tcweeei <open891013@gmail.com>
2024-03-03 21:20:38 +0800

    Edit the condition country invalidate.

--- utils/utils.go
+++ utils/utils.go
@@ -9,6 +9,8 @@ import (
 	"time"
 )
 
+const localLocation = "Local"
+
 // GetToday is a function to get today's date
 func GetToday() time.Time {
 	// get the location setting from the environment
@@ -17,7 +19,7 @@ func GetToday() time.Time {
 	// if failed to load location time, it will use local time to return
 	if err != nil {
 		now := time.Now()
-		today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, location)
+		today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 		return today
 	}
 
@@ -31,5 +33,9 @@ func GetToday() time.Time {
 func GetSqlDsnLocation() string {
 	// get the location and replace word "/" into "%2F"
 	location := EnvLocation()
-	return strings.Replace(location, "/", "%2F", 1)
+
+	if strings.ContainsAny(location, "/") {
+		return strings.Replace(location, "/", "%2F", 1)
+	}
+	return localLocation
 }
