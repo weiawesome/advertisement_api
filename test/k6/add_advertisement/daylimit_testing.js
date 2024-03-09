7af8feb651c6e7b9861e93c7430245e97dbca8f2
tcweeei <open891013@gmail.com>
2024-03-09 11:04:33 +0800

    Edit the test script for add advertisement.

--- test/k6/add_advertisement/daylimit_testing.js
+++ test/k6/add_advertisement/daylimit_testing.js
@@ -1,29 +1,32 @@
 import http from "k6/http";
-import { sleep,check } from "k6";
+import {check, sleep} from "k6";
 
 export const options = {
-    scenarios: {
-        contacts: {
-            executor: "ramping-arrival-rate",
-            startRate: 10,
-            timeUnit: "1s",
-            preAllocatedVUs: 10000,
-
-            stages: [
-                { target: 100, duration: "2s" },
-                { target: 100, duration: "2m" },
-                { target: 10, duration: "2s" },
-            ],
-        },
-    },
+    vus: 100,
+    iterations:2000,
 };
 
-function generateAdvertisement(){
-
+function generateAdvertisement(vu,iter){
+    return {
+        title: "Title vu : " + vu.toString() + " iter :" + iter.toString(),
+        startAt: new Date().toISOString(),
+        endAt: new Date(new Date().getTime() + 60 * 60 * 1000).toISOString(),
+        condition: {
+            ageStart: 20,
+            ageEnd: 30,
+            country: ["TW", "JP"],
+            platform: ["android", "ios"],
+            gender: ["M", "F"]
+        }
+    }
 }
 
 export default function () {
-    const requestUrl = __ENV.REQUEST_URL;
+    let requestUrl = __ENV.REQUEST_URL;
+
+    if (requestUrl===undefined){
+        requestUrl="http://127.0.0.1";
+    }
 
     const url = requestUrl+"/api/v1/ad"
 
@@ -32,17 +35,15 @@ export default function () {
             "Content-Type": "application/json",
         },
     };
-    const payload = {
-        title:"Title",
-        start_at:"2023-12-10T03:00:00.000Z",
-        end_at:"2025-12-31T16:00:00.000Z",
-    };
 
-    const response = http.post(url, JSON.stringify(payload), params);
+
+    const response = http.post(url, JSON.stringify(generateAdvertisement(__VU,__ITER)), params);
+
 
     check(response, {
         "Status is 200": (res) => res.status === 200,
     });
+    console.log(response.body)
 
     sleep(1);
 }
